2c36fdfdf4e5ff079021e6959008739775e41a1a
AlbertMukhammadiev <albert.mukhammadiev@gmail.com>
2019-02-25 16:45:03 +0300

    network tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,40 @@
 import pytest
+from layouts import SquareLayout, HexagonLayout
+from network import Network, NetworkGA
+from properties import PipeProps
 
 
-@pytest.fixture
+@pytest.fixture(scope='session')
 def square_layout():
-    return '123'
+    return SquareLayout(10, 10)
+
+
+@pytest.fixture(scope='session')
+def hexagon_layout():
+    return HexagonLayout(10, 10)
+
+
+@pytest.fixture(scope='session')
+def empty_network():
+    return Network(dict(), dict())
+
+
+@pytest.fixture(scope='session')
+def empty_ga_network():
+    return NetworkGA(dict(), dict())
+
+
+@pytest.fixture(scope='session')
+def cost_model():
+    return [
+        PipeProps(diameter=0.0, cost=0.0), PipeProps(diameter=80.0, cost=23.0),
+        PipeProps(diameter=100.0, cost=32.0), PipeProps(diameter=120.0, cost=50.0),
+        PipeProps(diameter=140.0, cost=60.0), PipeProps(diameter=160.0, cost=90.0),
+        PipeProps(diameter=180.0, cost=130.0), PipeProps(diameter=200.0, cost=170.0),
+        PipeProps(diameter=220.0, cost=300.0), PipeProps(diameter=240.0, cost=340.0),
+        PipeProps(diameter=260.0, cost=390.0), PipeProps(diameter=280.0, cost=430.0),
+        PipeProps(diameter=300.0, cost=470.0), PipeProps(diameter=320.0, cost=500.0),
+    ]
+
+
+
--- tests/test_network.py
+++ tests/test_network.py
@@ -1,27 +1,48 @@
 import pytest
-
-@pytest.fixture
-def network():
-    from network import Network
-    from data_context import DataContext
-
-    path = '../projects/square_layout/'
-    data_context = DataContext(path)
-    return Network(data_context)
+import math
+import random
 
 
-def test_total_cost(network):
-    individual = [
-        0,1,0,0,     # 1    
-        0,0,0,0,     # 2
-        0,0,1,1,     # 3
-        0,0,1,0,     # 4
-        0,0,0,1,     # 5
-        0,0,0,0,     # 6
-        1,1,0,0,     # 7
-        0,1,0,1,     # 8
-        0,0,0,0,     # 9
-        1,0,1,0,     # 10
-        0,0,1,1,     # 11
-        0,0,1,0]     # 12
-    assert network.total_cost(individual) == 907700
+def test_cost_model_mapping(empty_ga_network, cost_model):
+    empty_ga_network.cost_model = cost_model
+    mapped_model = empty_ga_network.cost_model
+    assert len(mapped_model) == 16
+    bits, pipe_props = random.choice(list(mapped_model.items()))
+    assert len(bits) == 4
+
+    sliced_model = cost_model[:8]
+    empty_ga_network.cost_model = sliced_model
+    mapped_model = empty_ga_network.cost_model
+    assert len(mapped_model) == len(sliced_model)
+    bits, pipe_props = random.choice(list(mapped_model.items()))
+    assert len(bits) == 3
+
+
+def test_cost_model_has0(empty_ga_network):
+    model = []
+    empty_ga_network.cost_model = model
+    model_values = list(empty_ga_network.cost_model.values())
+    diameters = [props.diameter for props in model_values]
+    assert 0 in diameters
+
+
+def test_total_cost(empty_ga_network, square_layout, cost_model):
+    assert empty_ga_network.total_cost() == 0
+    empty_ga_network.layout = square_layout
+    n_edges = len(square_layout.edges)
+    empty_ga_network.cost_model = cost_model
+
+    test_design = ['0000' for _ in range(n_edges)]
+    empty_ga_network.design = test_design
+    assert empty_ga_network.total_cost() == 0
+
+    for i in range(0, len(test_design), 3):
+        test_design[i] = '0001'
+    empty_ga_network.design = test_design
+    assert empty_ga_network.total_cost() == 1380
+
+    test_design = ['0001' for _ in range(n_edges)]
+    empty_ga_network.design = test_design
+    assert empty_ga_network.total_cost() == 4140
+
+    assert empty_ga_network._max_cost() == 90000
